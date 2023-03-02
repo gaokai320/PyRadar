de43275896c9033214caebccecf580a2068ff9db
Kai Gao <gaokaipku@qq.com>
2023-03-02 17:03:26 +0800

    Refactor PyPI and Package class for update use. * Allow to set whether to save data to files. * Allow to set whether to update data. This will check if the data file exists. If exists, load data from existing file. Else, query API and update data.

--- pypi_crawler.py
+++ pypi_crawler.py
@@ -4,6 +4,7 @@ import re
 import sys
 import time
 import xmlrpc.client
+from typing import List
 
 import requests
 
@@ -14,7 +15,6 @@ def safe_open(path):
 
 
 class PyPI:
-
     NAME = "PyPI"
     BASE_URL = "https://pypi.org"
 
@@ -34,79 +34,109 @@ class PyPI:
     """return the lastest 40 releases of the given package."""
 
     def __init__(self, data_folder: str = "/data/pypi_data") -> None:
+        """Initialize a PyPI object.
+
+        Args:
+            data_folder (str): the folder that store PyPI data. Defaults to "/data/pypi_data".
+        """
         self.data_folder = data_folder
 
-    def list_all_packages(self, api: str = "xmlrpc", filename=None, timeout: int = 60):
-        """Return names of all packages registered on PyPI.
+        self.name_file = os.path.join(data_folder, "names.json")
+        """The file that stores package names"""
+
+    def list_all_packages(
+        self,
+        api: str = "xmlrpc",
+        update: bool = True,
+        dump: bool = False,
+        timeout: int = 60,
+    ) -> List[str]:
+        """Return names of all packages registered on PyPI. Load form existing file or update the data.
 
         Args:
-            api (str, optional): the api used to retrieve packages, should be `xmlrpc` or `simple`. XML-RPC API returns more complete results than Simple API, but XML-RPC API will be deprecated in the future. Defaults to `xmlrpc`.
-            filename (str): file to save all package names. If None, will not save to file. Else, join with `self.data_folder`. Defaults to None.
-            timeout (int, optional): the timeout for requests.get. Defaults to 60.
-
+            api (str, optional): the api used to retrieve packages, should be "xmlrpc" or "simple". XML-RPC API returns more complete results than Simple API, but XML-RPC API will be deprecated in the future. Defaults to "xmlrpc".
+            update (bool): If False, load package names from `self.name_file` (if exists). Else, query PyPI API and update data. Defaults to True.
+            dump (bool): If True, dump package names to `self.name_file`. Defaults to False.
+            timeout (int, optional): the timeout for `requests.get`. Defaults to 60.
         """
         assert api in ["simple", "xmlrpc"], "api should be `xmlrpc` or `simple`"
-        pkgs = []
-        if filename is None:
-            file_path = None
-        else:
-            file_path = os.path.join(self.data_folder, filename)
-        ts = int(time.time())
-        if api == "xmlrpc":
-            pkgs = self.list_with_xmlrpc(ts, file_path)
-        elif api == "simple":
-            pkgs = self.list_with_simple(ts, file_path, timeout)
-        return pkgs
 
-    def list_with_xmlrpc(self, timestamp: int, file_path=None):
-        """Return names of all PyPI packages with XML-RPC API.
+        ## if `update`` is False and `slef.name_file` exists, load data from the file and return.
+        if (not update) and os.path.exists(self.name_file):
+            return list(json.load(open(self.name_file)).values())[0]
+
+        if api == "xmlrpc":
+            return self.list_with_xmlrpc(update, dump)
+        elif api == "simple":
+            return self.list_with_simple(update, dump, timeout)
+        return []
+
+    def list_with_xmlrpc(self, update: bool = True, dump: bool = False) -> List[str]:
+        """List packages names with XML-RPC API.
 
         Args:
-            timestamp (int): the timestamp when listing all packages.
-            file_path (str): file to store all package names. If None, will not save to file. Defaults to None.
+            update (bool): If False, load package names from `self.name_file` (if exists). Else, query XML-RPC API and update data. Defaults to True.
+            dump (bool): If True, dump package names to `self.name_file`. Defaults to False.
         """
-        pkgs = []
+
+        if (not update) and (os.path.exists(self.name_file)):
+            return list(json.load(open(self.name_file)).values())[0]
+
         try:
             client = xmlrpc.client.ServerProxy(PyPI.JSON_API_BASE)
+
+            # record current timestamp, for next package update use.
+            timestamp = time.time()
             pkgs = client.list_packages()
+
+            if dump:
+                with open(safe_open(self.name_file), "w") as f:
+                    json.dump({timestamp: pkgs}, f, indent=4)
+
+            return pkgs
         except Exception as e:
-            print("XML-RPC API query error!", e, file=sys.stderr)
+            print("[ERROR]: XML-RPC API query error!", e, file=sys.stderr)
 
-        if file_path is not None:
-            with open(safe_open(file_path), "w") as f:
-                json.dump({timestamp: pkgs}, f, indent=4)
+        return []
 
-        return pkgs
-
-    def list_with_simple(self, timestamp: int, file_path=None, timeout=60):
-        """Return names of all PyPI packages with Simple API.
+    def list_with_simple(
+        self, update: bool = True, dump: bool = False, timeout: int = 60
+    ) -> List[str]:
+        """List package names with Simple API.
 
         Args:
-            timestamp (int): the timestamp when listing all packages.
-            file_path (str): file to store all package names. If None, will not save to file. Defaults to None.
-            timeout (int): the timeout of requests.get. Defaults to 60.
+            update (bool): If False, load package names from `self.name_file` (if exists). Else, query Simple API and update data. Defaults to True.
+            dump (bool): If True, dump package names to `self.name_file`. Defaults to False.
+            timeout (int): the timeout of `requests.get`. Defaults to 60.
         """
-        pkgs = []
-        url = PyPI.SIMPLE_API_BASE_URL
+
+        if (not update) and (os.path.exists(self.name_file)):
+            pkgs = list(json.load(open(self.name_file)).values())[0]
+            return pkgs
+
         try:
-            response = requests.get(url, timeout=timeout)
+            # record current timestamp, for next package update use.
+            timestamp = time.time()
+
+            response = requests.get(PyPI.SIMPLE_API_BASE_URL, timeout=timeout)
             response.raise_for_status()
             content = response.text
             pattern = r"<a href=.*>(.*?)</a>"
             pkgs = re.findall(pattern, content)
+
+            if dump:
+                with open(safe_open(self.name_file), "w") as f:
+                    json.dump({timestamp: pkgs}, f, indent=4)
+
+            return pkgs
         except Exception as e:
-            print("Simple API query error!", e, file=sys.stderr)
-
-        if file_path is not None:
-            with open(safe_open(file_path), "w") as f:
-                json.dump({timestamp: pkgs}, f, indent=4)
-
-        return pkgs
+            print("[ERROR]: Simple API query error!", e, file=sys.stderr)
+            return []
 
 
 class Package:
     def __init__(self, name: str, data_folder="/data/pypi_data/metadata") -> None:
-        """Initialize a PyPI Package object with the name
+        """Initialize a PyPI Package object with package name.
 
         Args:
             name (str): the PyPI package's name
@@ -115,37 +145,61 @@ class Package:
         self.name = name
         self.data_folder = os.path.join(data_folder, name)
 
-    def get_versions(self, tofile=0):
-        """Return all versions of the package. This piece information can be accessed by both JSON API and XML-RPC API. Since XML-RPC API will be deprecated, here only uses the JSON API."""
-        url = PyPI.JSON_API_BASE + f"/{self.name}/json"
+    def get_versions(self, update: bool = True, dump: bool = False):
+        """Return all versions of the package. This piece information can be accessed by both JSON API and XML-RPC API. Since XML-RPC API will be deprecated, here only uses the JSON API.
+
+        Args:
+            update(bool): If False, load package metadata from the `{self.name}.json` file in `self.data_folder` (if exists). Else, query Simple API and update data. Defaults to True.
+            dump (bool): If True, dump package metadata to the `{self.name}.json` file in `self.data_folder`. Defaults to False.
+        """
+        file_path = os.path.join(self.data_folder, f"{self.name}.json")
+
+        if (not update) and os.path.exists(file_path):
+            data = json.load(open(file_path))
+            return list(data["releases"].keys())
+
         try:
-            r = requests.get(url)
+            r = requests.get(PyPI.JSON_API_BASE + f"/{self.name}/json")
             r.raise_for_status()
             data = r.json()
-            if tofile == 1:
-                file_path = os.path.join(self.data_folder, f"{self.name}.json")
+            if dump:
                 with open(safe_open(file_path), "w") as f:
                     json.dump(data, f, indent=4)
-            versions = list(data["releases"].keys())
-            return versions
+            return list(data["releases"].keys())
         except Exception as e:
-            print(f"Package metadata query for {self.name} error!", e, file=sys.stderr)
+            print(
+                f"[ERROR]: Package metadata query for {self.name} error!",
+                e,
+                file=sys.stderr,
+            )
+            return []
 
-    def query_single_release(self, version: str, tofile=0):
+    def query_single_release(self, version: str, dump: bool = False):
         """Query the metadata of the given version
 
         Args:
-            version (str): the version to query metadata
+            version (str): the version to query metadata.
+            dump (bool): If True, dump package metadata to the `{version}.json` file in `self.data_folder`. Defaults to False.
         """
-        url = PyPI.JSON_API_BASE + f"/{self.name}/{version}/json"
+
+        file_path = os.path.join(self.data_folder, f"{version}.json")
+
+        if os.path.exists(file_path):
+            data = json.load(open(file_path))
+            print(f"[INFO]: {file_path} already exist!", file=sys.stderr)
+            return data
+
         try:
-            r = requests.get(url)
+            r = requests.get(PyPI.JSON_API_BASE + f"/{self.name}/{version}/json")
             r.raise_for_status()
             data = r.json()
-            if tofile == 1:
-                file_path = os.path.join(self.data_folder, f"{version}.json")
+            if dump:
                 with open(safe_open(file_path), "w") as f:
                     json.dump(data, f, indent=4)
             return data
         except Exception as e:
-            print(f"Release metadata query for {self.name} {version} error!", e, file=sys.stderr)
+            print(
+                f"[ERROR]: Release metadata query for {self.name} {version} error!",
+                e,
+                file=sys.stderr,
+            )
