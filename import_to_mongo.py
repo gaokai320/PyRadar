d554dcc797dc68443ff5f2d333bb88c0d60de8f8
Kai Gao <gaokaipku@qq.com>
2023-03-08 21:09:50 +0800

    import release metadata to mongodb

---
+++ import_to_mongo.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import json
+from tqdm import tqdm
+
+import pymongo
+from pymongo import MongoClient
+from pymongo.write_concern import WriteConcern
+
+db = MongoClient("127.0.0.1", 27017)["pypi"]
+col = db["release_metadata"]
+
+metadata_folder = sys.argv[1]
+packages = list(os.listdir(metadata_folder))
+
+info_keys = [
+    "name",
+    "version",
+    "summary",
+    "keywords",
+    "license",
+    "author",
+    "author_email",
+    "maintainer",
+    "maintainer_email",
+    "home_page",
+    "download_url",
+    "project_urls",
+    "requires_dist",
+    "classifiers",
+    "description",
+    "description_content_type",
+    "bugtrack_url",
+    "docs_url",
+    "downloads",
+    "package_url",
+    "platform",
+    "project_url",
+    "release_url",
+    "requires_python",
+    "yanked",
+    "yanked_reason",
+]
+
+batch = []
+for pkg in tqdm(packages):
+    for version in os.listdir(os.path.join(metadata_folder, pkg)):
+        if version == f"{pkg}.json":
+            continue
+        try:
+            metadata = json.load(open(os.path.join(metadata_folder, pkg, version)))
+            info_data = metadata["info"]
+            data = {}
+            for key in info_keys:
+                data[key] = info_data.get(key, None)
+            if len(metadata["urls"]) > 0:
+                data["upload_time"] = metadata["urls"][0]["upload_time"]
+            batch.append(data)
+        except Exception as e:
+            print(f"{pkg} {version}: error! {e}")
+        if len(batch) % 1000 == 0:
+            try:
+                col.with_options(write_concern=WriteConcern(w=0)).insert_many(
+                    batch, ordered=False
+                )
+            except pymongo.errors.BulkWriteError as e:
+                pass
+if len(batch) > 0:
+    col.with_options(write_concern=WriteConcern(w=0)).insert_many(batch, ordered=False)
